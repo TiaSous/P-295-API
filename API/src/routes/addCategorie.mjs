f9d86eb7ee6fffc8af7e46fe72eab805e03802bc
EvinPara <96207677+Evin06@users.noreply.github.com>
2024-03-04 09:28:48 +0100

    Ajout des routes catégorie

---
+++ API/src/routes/addCategorie.mjs
@@ -0,0 +1,18 @@
+import express from "express";
+import { success } from "./helper.mjs";
+import { Categorie } from "../db/sequelize.mjs";
+
+const addCategorie = express();
+
+addCategorie.post("/", (req, res) => {
+    const infoCategorie = {...req.body};
+    Categorie.create(infoCategorie).then((categorie) => {
+        const message = `La catégorie avec l'ID ${categorie.id_categorie} a bien été créée !`;
+        res.json(success(message, categorie));
+    }).catch((error) => {
+        const message = "La catégorie n'a pas pu être ajoutée. Merci de réessayer dans quelques instants.";
+        res.status(500).json({ message, data: error });
+    });
+});
+
+export { addCategorie };                        
